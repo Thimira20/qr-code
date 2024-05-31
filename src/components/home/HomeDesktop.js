361d4dc61d56b38ac542d35bf6d9fd66dbda839d
thimira <thimirapanditha20@gmail.com>
2024-06-01 02:01:09 +0530

    vertion 1

---
+++ src/components/home/HomeDesktop.js
@@ -0,0 +1,148 @@
+import styles from "./HomeDesktop.module.css";
+
+const HomeDesktop = () => {
+  return (
+    <div className={styles.homeDesktop}>
+      <div className={styles.homeDesktopChild} />
+      <div className={styles.homeDesktopItem} />
+      <div className={styles.homeDesktopInner} />
+      <div className={styles.rectangleDiv} />
+      <div className={styles.homeDesktopChild1} />
+      <div className={styles.homeParent}>
+        <b className={styles.home}>{`Home `}</b>
+        <b className={styles.qrGenerator}>{`QR Generator  `}</b>
+        <div className={styles.contact}>
+          <b className={styles.contact1}>Contact</b>
+          <span className={styles.span}>{` `}</span>
+        </div>
+      </div>
+      <div className={styles.homeGroup}>
+        <b className={styles.home}>{`Home `}</b>
+        <b className={styles.home}>Steps</b>
+        <b className={styles.qrGenerator}>{`QR Generator  `}</b>
+        <div className={styles.contact}>
+          <b className={styles.contact1}>Contact</b>
+          <span className={styles.span}>{` `}</span>
+        </div>
+      </div>
+      <div className={styles.logo}>
+        <img
+          className={styles.scanQrCode31Icon}
+          alt=""
+          src="/image/scan-qr-code-3 1.png"
+        />
+        <div className={styles.easyQr}>
+          <p className={styles.easy}>easy</p>
+          <p className={styles.easy}> QR</p>
+        </div>
+      </div>
+      <b className={styles.welcomeToTheContainer}>
+        <p className={styles.easy}>{`Welcome `}</p>
+        <p className={styles.easy}>{`to the QR `}</p>
+        <p className={styles.easy}>{`World `}</p>
+      </b>
+      <div className={styles.qrCode14873212801Wrapper}>
+        <img
+          className={styles.qrCode14873212801Icon}
+          alt=""
+          src="/image/qr-code-148732_1280 1.png"
+        />
+      </div>
+      <div className={styles.qrCode14873212801Container}>
+        <img
+          className={styles.qrCode14873212801Icon1}
+          alt=""
+          src="/image/qr-code-148732_1280 1.png"
+        />
+      </div>
+      <div className={styles.qrCode14873212801Frame}>
+        <img
+          className={styles.qrCode14873212801Icon2}
+          alt=""
+          src="/image/qr-code-148732_1280 1.png"
+        />
+      </div>
+      <b className={styles.followTheFollowing}>Follow the following steps</b>
+      <b className={styles.generateYourOwn}>Generate your own QR code</b>
+      <b className={styles.contactUs}>Contact Us</b>
+      <div className={styles.frameDiv}>
+        <div className={styles.frameWrapper}>
+          <div className={styles.frameParent}>
+            <div className={styles.pasteYourLinkWrapper}>
+              <b className={styles.pasteYourLink}>{`Paste your link `}</b>
+            </div>
+            <img className={styles.buttonIcon} alt="" src="/image/button.svg" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.homeDesktopInner1}>
+        <div className={styles.frameWrapper}>
+          <div className={styles.frameParent}>
+            <div className={styles.pasteYourLinkWrapper}>
+              <b className={styles.pasteYourLink}>{`Paste your link `}</b>
+            </div>
+            <img className={styles.buttonIcon} alt="" src="/image/button.svg" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.homeDesktopInner2}>
+        <div className={styles.frameWrapper}>
+          <div className={styles.frameParent}>
+            <div className={styles.pasteYourLinkWrapper}>
+              <b className={styles.pasteYourLink}>{`Paste your link `}</b>
+            </div>
+            <img className={styles.buttonIcon} alt="" src="/image/button.svg" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.homeDesktopInner3}>
+        <div className={styles.frameWrapper}>
+          <div className={styles.frameParent}>
+            <div className={styles.pasteYourLinkWrapper}>
+              <b className={styles.pasteYourLink}>{`Paste your link `}</b>
+            </div>
+            <img className={styles.buttonIcon} alt="" src="/image/button.svg" />
+          </div>
+        </div>
+      </div>
+      <b className={styles.b}>01</b>
+      <b className={styles.b1}>01</b>
+      <b className={styles.b2}>01</b>
+      <b className={styles.b3}>01</b>
+      <div className={styles.dbd0b51c20a695901a84c4c083500fParent}>
+        <img
+          className={styles.dbd0b51c20a695901a84c4c083500fIcon}
+          alt=""
+          src="/image/7dbd0b51c20a695901a84c4c083500f6 1.png"
+        />
+        <img
+          className={styles.freeLinkedinIcon112Thumb1}
+          alt=""
+          src="/image/free-linkedin-icon-112-thumb 1.png"
+        />
+        <img
+          className={styles.githubLogo7880d80b8dSeeklogIcon}
+          alt=""
+          src="/image/github-logo-7880D80B8D-seeklogo 1.png"
+        />
+        <img
+          className={styles.freeLinkedinIcon112Thumb1}
+          alt=""
+          src="/image/logo-facebook 1 1.png"
+        />
+        <img
+          className={styles.freeLinkedinIcon112Thumb1}
+          alt=""
+          src="/image/logo-tiktok 1 1.png"
+        />
+      </div>
+      <img className={styles.images1Icon} alt="" src="/image/images 1.png" />
+      <div className={styles.homeDesktopChild2} />
+      <div className={styles.homeDesktopChild3} />
+      <div className={styles.generate}>Generate</div>
+      <div className={styles.pasteYourLink4}>{`paste your link here `}</div>
+    </div>
+  );
+};
+
+export default HomeDesktop;
